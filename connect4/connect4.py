5df8a29916428055f71382a358c1cf4ffc0bef1b
Susruth Puppala <abhirama99@live.com>
2024-05-19 19:24:29 +0100

    Added indicies on grid and functions to make it easier

--- connect4/connect4.py
+++ connect4/connect4.py
@@ -4,16 +4,9 @@ from array import *
 horizontal = 6
 vertical = 7
 wonGame = False
-counter = 0
+
 
 grid = [["." for columns in range(horizontal)] for rows in range(vertical)]
-
-
-"""for i in range(len(grid)):
-    for y in range(len(grid[i])):
-        print(grid[i][y], end=" ")
-    print()"""
-
 
 
 
@@ -24,14 +17,14 @@ print("\n")
 
 def CheckIfGameWon(input):
 
-    for i in range(len(grid)):
-        for y in range(len(grid[i])):
+    for i in range(horizontal):
+        for y in range(vertical -3):
             if grid[i][y] == input and grid[i][y+1] == input and grid[i][y+2] == input and grid[i][y+3] == input:
                 print("\nGame Over Thank you for playing")
                 return True
 
-    for i in range(len(grid)):
-        for y in range(len(grid[i])):
+    for i in range(horizontal):
+        for y in range(vertical-3):
             if grid[i][y] == input and grid[i+1][y] == input and grid[i+1][y] == input and grid[i+1][y] == input:
                 print("\nGame Over Thank you for playing")
                 return True
@@ -39,10 +32,17 @@ def CheckIfGameWon(input):
 
 def printStateOfBoard(board):
     print("Updating elements")
-    for i in range(len(board)):
+    # Print the column indices
+    print("   " + " ".join(f"{i}" for i in range(horizontal)))
+
+    # Print the board with row indices
+    for i in range(vertical):
+        print(f"{i}  " + " ".join(board[i]))
+
+    """for i in range(len(board)):
         for y in range(len(board[i])):
-            print(board[i][y], end=" ")
-        print()
+            print(board[i][y], end=" ")"""
+        #print()
 
 def placeCounter(turn):
     rowChosen = int(input("What row do you want? \n"))
@@ -51,21 +51,25 @@ def placeCounter(turn):
 
 ## game loop
 
-while(wonGame == False):
-    if counter % 2 == 0:
-        printStateOfBoard(grid)
-        while True:
-            print("Player 1 turn \n")
-            ## update elements
-            placeCounter("*")
-            CheckIfGameWon(grid)
-            counter += 1
-            break
-    else:
-        printStateOfBoard(grid)
-        while True:
-            print("Player 2 turn \n")
-            placeCounter("0")
-            CheckIfGameWon(grid)
-            counter += 1
-            break
+def GameLoop():
+    printStateOfBoard(grid)
+    counter = 0
+    while(wonGame == False):
+        if counter % 2 == 0:
+            while True:
+                print("Player 1 turn \n")
+                ## update elements
+                placeCounter("*")
+                CheckIfGameWon(grid)
+                counter += 1
+                break
+        else:
+            printStateOfBoard(grid)
+            while True:
+                print("Player 2 turn \n")
+                placeCounter("0")
+                CheckIfGameWon(grid)
+                counter += 1
+                break
+
+GameLoop()
