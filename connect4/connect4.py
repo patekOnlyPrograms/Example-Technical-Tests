6c00288ca1147c0e2cf5fcc18aa8bd733017cce1
Susruth Puppala <abhirama99@live.com>
2024-05-21 14:38:16 +0100

    Think i have completed project

--- connect4/connect4.py
+++ connect4/connect4.py
@@ -1,8 +1,8 @@
 ## Grid making
 from array import *
 
-horizontal = 6
-vertical = 7
+horizontal = 7
+vertical = 6
 wonGame = False
 
 
@@ -10,28 +10,28 @@ grid = [["." for columns in range(horizontal)] for rows in range(vertical)]
 
 
 
-print("\n")
-print("\n")
+"""print("\n")
+print("\n")"""
 
 ## Winning via the row
 
 def CheckIfGameWon(input):
 
-    for i in range(horizontal):
-        for y in range(vertical -3):
-            if grid[i][y] == input and grid[i][y+1] == input and grid[i][y+2] == input and grid[i][y+3] == input:
-                print("\nGame Over Thank you for playing")
+    # vertical win
+    for row in range(vertical):
+        for col in range(horizontal -3):
+            if grid[row][col] == input and grid[row][col+1] == input and grid[row][col+2] == input and grid[row][col+3] == input:
                 return True
 
-    for i in range(horizontal):
-        for y in range(vertical-3):
-            if grid[i][y] == input and grid[i+1][y] == input and grid[i+1][y] == input and grid[i+1][y] == input:
-                print("\nGame Over Thank you for playing")
+    # horizontal win
+    for row in range(vertical - 3):
+        for col in range(horizontal):
+            if grid[row][col] == input and grid[row + 1][col] == input and grid[row + 2][col] == input and grid[row + 3][col] == input:
                 return True
     return False
 
 def printStateOfBoard(board):
-    print("Updating elements")
+    print("Current state of board")
     # Print the column indices
     print("   " + " ".join(f"{i}" for i in range(horizontal)))
 
@@ -45,31 +45,40 @@ def printStateOfBoard(board):
         #print()
 
 def placeCounter(turn):
-    rowChosen = int(input("What row do you want? \n"))
-    ColumnChosen = int(input("What column do you want? \n"))
-    grid[rowChosen][ColumnChosen] = turn
+    while True:
+        rowChosen = int(input("What row do you want? \n"))
+
+        ColumnChosen = int(input("What column do you want? \n"))
+        if grid[rowChosen][ColumnChosen] != ".":
+            print("Position is already used")
+        else:
+            grid[rowChosen][ColumnChosen] = turn
+            break
+
+    
 
 ## game loop
 
 def GameLoop():
+    global wonGame
     printStateOfBoard(grid)
     counter = 0
-    while(wonGame == False):
+    while not wonGame:
         if counter % 2 == 0:
-            while True:
-                print("Player 1 turn \n")
-                ## update elements
-                placeCounter("*")
-                CheckIfGameWon(grid)
-                counter += 1
-                break
+            print("Player 1 turn \n")
+            ## update elements
+            placeCounter("*")
+            if CheckIfGameWon("*"):
+                print("\nGame Over Thank you for playing, Player 1 wins!")
+                wonGame = True
         else:
-            printStateOfBoard(grid)
-            while True:
-                print("Player 2 turn \n")
-                placeCounter("0")
-                CheckIfGameWon(grid)
-                counter += 1
-                break
+            print("Player 2 turn \n")
+            placeCounter("0")
+            if CheckIfGameWon("0"):
+                print("\nGame Over Thank you for playing, Player 2 wins!")
+                wonGame = True
+
+        counter += 1
+        printStateOfBoard(grid)
 
 GameLoop()
